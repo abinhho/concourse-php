b9de492b965fe8311868bbf19ee39445181199ad
hbinh <hbinh@yahoo-corp.jp>
2017-08-23 12:53:32 +0700

    test

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -2,6 +2,17 @@
 
 composer install
 composer dump-autoload
+echo "$ pwd"
+pwd
+echo "$ ls -al"
+ls  -al
+echo "$ ls -al vendor"
+ls vendor
+echo "$ ls -al vendor/bin"
+ls vendor/bin
 
 mkdir ../source
 cp -r . ../source
+
+echo "ls ../source -al"
+ls ../source -al
--- pipeline/test.sh
+++ pipeline/test.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
+echo "$ pwd"
+pwd
+echo "$ ls -al"
+ls  -al
+echo "$ ls -al vendor"
+ls vendor
+echo "$ ls -al vendor/bin"
+ls vendor/bin
 
 cd built-concourse-php
 php vendor/bin/phpunit --log-junit ../test-results/results.xml --testdox-text ../test-results/testdox.txt
